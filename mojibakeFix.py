004dfea21693224b44db93e96967dc0365ab32e6
loromichi <loromichi@gmail.com>
2014-11-16 20:10:21 +0900

    main処理の追加

--- mojibakeFix.py
+++ mojibakeFix.py
@@ -1,2 +1,17 @@
 __author__ = 'loromichi'
 
+from decoder import UTF_8
+
+
+def main():
+    with open("out.txt", "rb") as f:
+        lines = f.read()
+    print(lines.decode("UTF-8", errors="replace"))
+
+    u = UTF_8()
+    fixed_string = u.fix_mojibake(lines, b"\x81\x45")
+    print(fixed_string)
+
+
+if __name__ == '__main__':
+    main()
