f3e92c8edb179a34ba32007a9df95398984d919b
loromichi <loromichi@gmail.com>
2014-11-17 20:17:27 +0900

    ファイルを引数で入れるようにした，pickleファイルがない場合作成するようにした
    pickleファイルがない場合はsettingで作成していたけど，mainでやるようにした

--- mojibakeFix.py
+++ mojibakeFix.py
@@ -1,12 +1,32 @@
 __author__ = 'loromichi'
 
+import os
+import argparse
+import tools
 from decoder import UTF_8
 
 
 def main():
-    with open("sample.txt", "rb") as f:
+    # 引数の処理
+    parser = argparse.ArgumentParser(description='fix mojibake')
+    parser.add_argument('-f', type=str, help='text file')
+
+    args = parser.parse_args()
+    input_file = args.f
+
+    if not os.path.exists(input_file):
+        print(input_file, "is not found")
+        return
+
+    # データの作成(ないときだけ)
+    if not os.path.exists(os.path.join("pickles", "utf8_charset.pickle")):
+        tools.make_utf8_char_set()
+    if not os.path.exists(os.path.join("pickles", "ngram.pickle")):
+        tools.n_gram()
+
+    # 文字化けの復元
+    with open(input_file, "rb") as f:
         lines = f.read()
-    print(lines.decode("UTF-8", errors="replace"))
 
     u = UTF_8()
     fixed_string = u.fix_mojibake(lines, b"\x81\x45")
